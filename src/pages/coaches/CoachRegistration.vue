15fd0cdeed11db7ee9019507cd8df32b0fe73efe
seif essam <seifessam60@gmail.com>
2025-01-09 11:23:04 +0200

    add coach form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,1 +1,18 @@
-<template><section>Register as a Coach</section></template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a Coach</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+
+export default {
+  components: {
+    CoachForm,
+  },
+};
+</script>
